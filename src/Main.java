a257d3682540c08fbfb418f6de4b53ae2051ea23
Nasyidashvili <NikolozNaskidashvili@gmail.com>
2025-11-24 02:15:27 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("I like khinkali!");
+        System.out.println("Its really good!");
+        System.out.println("Buy me pizza!");
+    }
+}
