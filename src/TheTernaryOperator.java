709232ad1fa60dc489cf3f415abf5646aebb0d26
Nikoloz Naskidashvili <NikolozNaskidashvili@gmail.com>
2025-12-07 00:45:55 +0400

    my projects second time

--- src/TheTernaryOperator.java
+++ src/TheTernaryOperator.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public record TheTernaryOperator() {
+    public static String describeAge(int age) {
+//        if (age <= 12) {
+//            return "You're a(n) kid";
+//        } else if (age >= 13 && age <= 17) {
+//            return "You're a(n) teenager";
+//        } else if (age >= 18 && age <= 64) {
+//            return "You're a(n) adult";
+//        } else {
+//            return "You're a(n) elderly";
+//        }
+        age = 12;
+        return "You are a(n) " + (age <= 12 ? "kid" : age >= 13 && age <= 17 ? "teenager" : age >= 18 && age <= 64 ? "adult" : "elderly");
+    }
 }
--- src/rockPaperScissors.java
+++ src/rockPaperScissors.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class rockPaperScissors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String player1;
+        String player2;
+
+        System.out.print("Player1 Make a move (Rock, Scissors, Paper):");
+        player1 = scanner.nextLine().toLowerCase().trim();
+
+        System.out.print("Player2 Make a move (Rock, Scissors, Paper):");
+        player2 = scanner.nextLine().toLowerCase().trim();
+
+        if (player1.equals("rock") && player2.equals("scissors")) {
+            System.out.println("Player 1 wins!");
+        }
+        else if (player1.equals("scissors") && player2.equals("paper")) {
+            System.out.println("Player 1 wins!");
+        }
+        else if (player1.equals("paper") && player2.equals("rock")) {
+            System.out.println("Player 1 wins!");
+        }
+
+        else if (player2.equals("rock") && player1.equals("scissors")) {
+            System.out.println("Player 2 wins!");
+        }
+        else if (player2.equals("scissors") && player1.equals("paper")) {
+            System.out.println("Player 2 wins!");
+        }
+        else if (player2.equals("paper") && player1.equals("rock")) {
+            System.out.println("Player 2 wins!");
+        }
+        else if (player1.equals(player2)) {
+            System.out.println("It's draw!");
+        }
+
+        scanner.close();
+    }
 }
