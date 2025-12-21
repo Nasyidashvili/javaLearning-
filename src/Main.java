0cde7c9ea09d421b940c0fc745e17c0ed5a3c920
Nikoloz Naskidashvili <NikolozNaskidashvili@gmail.com>
2025-12-21 20:54:28 +0400

    added project quizGame

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,11 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 
 public class Main {
-
-
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-
-            int s = a;
-
-            for(int j = 0; j<n;j++){
-                s = a + (int) Math.pow(2, j) * b;
-                System.out.print(s + " ");
-            }
-            System.out.println();
-        }
-        in.close();
     }
 }
 
